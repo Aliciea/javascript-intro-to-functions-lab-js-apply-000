104d5418023879432a4fabf9fd6976a940a1dd22
Aliciea D Suechin <adr330@nyu.edu>
2019-06-02 18:57:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ function whisper(string) {
 }
 function sayHiToGrandma(string) {
   return "I can\'t hear you!";
-}
-function sayHiToGrandma(string) {
-  return "I love you, too."
+  return "YES INDEED!"
 }
 
